5660a81125610fa6d377c4a1a3b740c3921eaa46
Will Hallam <william.hallam@servicenow.com>
2022-09-14 17:28:12 -0400

    add scale script

---
+++ scale.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import json
+import subprocess
+import sys
+
+# initialize
+incr=1
+
+# parse args
+serviceName=sys.argv[1]
+upDown=sys.argv[2]
+
+if (len(sys.argv)>3):
+    incr=sys.argv[3]
+
+
+# read config file
+
+# cluster name from config file
+
+# update Kubeconfig
+updateKubeconfig=subprocess.run(["aws","--region","us-east-1","eks","update-kubeconfig","--name","hallam-1"])
+
+# retrieve JSON for existing object
+getResource=subprocess.run(["kubectl","get","-n","demo-project","-f",serviceName+".yaml","-o","json"],stdout=subprocess.PIPE,encoding='UTF-8')
+
+print(getResource.stdout)
+serviceJson=json.loads(getResource.stdout)
+if (upDown=="up"):
+    newReplicas=serviceJson["spec"]["replicas"]+1
+if (upDown=="down"):
+    newReplicas=serviceJson["spec"]["replicas"]-1
+
+if (newReplicas>0):
+    kubeScale=subprocess.run(["kubectl","scale","-n","demo-project","--replicas",str(newReplicas),"-f",serviceName+".yaml"],stdout=subprocess.PIPE,stderr=subprocess.PIPE,encoding='UTF-8')
+    print (kubeScale.stdout)
